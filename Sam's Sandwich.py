b52094498fd1fb572eaec136445b5e6e2677043a
egam22090-ctrl <egam.22090@obhs.school.nz>
2025-08-29 09:49:31 +1200

    I created my first 3 functions and added them to my main menu

--- Sam's Sandwich.py
+++ Sam's Sandwich.py
@@ -1,1 +1,40 @@
-#Sams's Sandwich
+#This program runs a sandwich ordering service like 'Subway'
+def bread_selection():
+    bread_list = ["White", "Brown", "Italian", "Germany"]
+    count=0
+    print("We have the following breads:")
+    while count < len(bread_list):
+        print(count+1,"",bread_list[count])
+        count+=1
+    bread_selected=int(input("WHich bread do you want? Enter a number: "))
+    return bread_list[bread_selected-1]
+
+def meat_selection():
+    meat_list = ["None","Pork", "Turkey", "Chicken", "Meatballs"]
+    count=0
+    print("We have the following meats:")
+    while count < len(meat_list):
+        print(count+1,"",meat_list[count])
+        count+=1
+    meat_selected=int(input("Which bread do you want? Enter a number: "))
+    return meat_list[meat_selected-1]
+
+def cheese_selection():
+    cheese_list = ["None","Old English", "Pale Triangle", "Mozzerela", "Cheddar"]
+    count=0
+    print("We have the following cheeses:")
+    while count < len(cheese_list):
+        print(count+1,"",cheese_list[count])
+        count+=1
+    cheese_selected=int(input("Which bread do you want? Enter a number: "))
+    return cheese_list[cheese_selected-1]
+
+
+#main program
+print("Welcome to Sam's Sandwich Shop")
+bread_choice=bread_selection()
+print(f"Your selected  bread: {bread_choice}")
+meat_choice=meat_selection()
+print(f"Your selected meat was: {meat_choice}")
+cheese_choice=cheese_selection()
+print(f"Your selcted bread: {cheese_choice}")
